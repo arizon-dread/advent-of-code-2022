0eda52ef10f9fcfcb8bba9bb6d6f996c99dab805
Erik Svensson <arizon.es@gmail.com>
2023-12-05 22:15:54 +0100

    at least I have gathered the numbers into an array :D

--- 2023/3/main.go
+++ 2023/3/main.go
@@ -8,8 +8,6 @@ import (
 	"unicode"
 )
 
-var total int = 0
-
 func main() {
 	f, err := os.ReadFile("input.txt")
 
@@ -23,7 +21,7 @@ func main() {
 
 	specialChars := make([]map[int]string, 140)
 	numbers := make([]map[int]string, 140)
-
+	var total int = 0
 	//loop over each line in turn
 	for i, l := range sArr {
 		specialChars[i] = make(map[int]string, 10)
@@ -38,11 +36,11 @@ func main() {
 				numbers[i][j] = string(char)
 			nextChar:
 				for k := 1; k < len(l); k++ {
-					if j+k > len(l) {
+					if j+k >= len(l) {
 						break
 					}
 					if unicode.IsDigit(rune(l[j+k])) {
-						numbers[i][j] += string(char)
+						numbers[i][j] += string(l[j+k])
 					} else {
 						//increment j but subtract one to accomodate for j++
 						j = j + k - 1
@@ -50,30 +48,43 @@ func main() {
 					}
 				}
 			} else {
+				fmt.Printf("found a special char! %c\n", char)
 				specialChars[i][j] = string(char)
 			}
 		}
 
 	}
 	for i, slc := range numbers {
+
 		for j, v := range slc {
-			if _, exists := specialChars[i-1][j]; exists {
-				addStrToTotal(v)
-			} else if _, exists := specialChars[i+1][j]; exists {
-				addStrToTotal(v)
-			} else if _, exists := specialChars[i][j-1]; exists {
-				addStrToTotal(v)
-			} else if _, exists := specialChars[i][j+len(v)+1]; exists {
-				addStrToTotal(v)
+			//fmt.Printf("found a number! %v\n", v)
+			valid := false
+			if i > 0 && j > 0 && len(specialChars[i]) >= j+len(v)+1 {
+				if _, exists := specialChars[i-1][j]; exists {
+					valid = true
+				}
+				if _, exists := specialChars[i+1][j]; exists {
+					valid = true
+				}
+				if _, exists := specialChars[i][j-1]; exists {
+					valid = true
+				}
+				if _, exists := specialChars[i][j+len(v)+1]; exists {
+					valid = true
+				}
+			}
+			if valid {
+				total += addStrToTotal(v)
 			}
 		}
 	}
 	fmt.Printf("total: %v\n", total)
 }
 
-func addStrToTotal(s string) {
+func addStrToTotal(s string) int {
 	n, err := strconv.Atoi(s)
 	if err == nil {
-		total += n
+		return n
 	}
+	return 0
 }
