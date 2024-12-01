32d796e6407c7fd7c4305362497176e33bac8532
Erik Svensson <arizon.es@gmail.com>
2024-12-01 18:17:43 +0100

    remove todo

--- 2024/1/listcomparer_test.go
+++ 2024/1/listcomparer_test.go
@@ -16,7 +16,7 @@ func Test_compareLists(t *testing.T) {
 		args args
 		want int
 	}{
-		{"sample a", sample_input, 11}, // TODO: Add test cases.
+		{"sample a", sample_input, 11},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
